f5465e8d3a4b6fe2ea077b7b3b615b125c121e8c
keddad youcef <keddad06@gmail.com>
2022-11-10 22:32:42 +0100

    ADD UNROLL8 function

--- dotprod/main.c
+++ dotprod/main.c
@@ -48,6 +48,7 @@ int main(int argc, char **argv)
 	 "n", "r", "d", "min", "max", "mean", "stddev (%)", "MiB/s");
   
   run_benchmark("BASE",   dotprod_base, n, r);
+  run_benchmark("UNROLL8",   dotprod_unroll_8, n, r);  
   
   //
   return 0;
