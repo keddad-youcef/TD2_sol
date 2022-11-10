b8b5f0ee8c401ce5509244d5f057c2bcc95b0b83
keddad youcef <keddad06@gmail.com>
2022-11-10 23:03:09 +0100

    ADD UNROLL8 function

--- reduc/kernels.c
+++ reduc/kernels.c
@@ -19,3 +19,26 @@ f64 reduc_base(f64 *restrict a, u64 n)
   return d;
 }
 
+
+f64 reduc_unroll_8(f64 *restrict a, u64 n)
+{
+  #define UNROLL8 8 
+  double d = 0.0;
+  
+  for (u64 i = 0; i < (n - (n & (UNROLL8 - 1))); i += UNROLL8)
+    {
+        d += a[i];
+        d += a[i+1];
+        d += a[i+2];
+        d += a[i+3];
+        d += a[i+4];
+        d += a[i+5];
+        d += a[i+6];
+        d += a[i+7];
+        
+    }
+    //Manage the leftovers
+	  for (u64 j = (n - (n & 7)); j < n; j++)
+	    d +=  a[j];
+  return d;
+}
